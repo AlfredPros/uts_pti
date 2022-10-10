b8b2bc0b8e8974677c6c95e104873ea3ff4e8f0d
IsRylo <91057829+IsRylo@users.noreply.github.com>
2022-10-10 21:28:54 +0800

    Apa yang slaah???

--- js/table_game.js
+++ js/table_game.js
@@ -4,9 +4,24 @@ let spacing = 1.0 / num;
 let temp = "";
 let id = 0;
 
-for (let i=0; i<num+2; i++) {
+let dangerous_boners = [];
 
+// Function to select random bones
+function random_bones() {
+    for (let i = 0; dangerous_boners.length < num; i++) {
+        // Generate Random Number between 0 and 4*number of players
+        const x =   Math.floor(Math.random() * (4 * num));
+        // Check if the number is already in danger_bones
+        if (!dangerous_boners.includes(x)) {
+            dangerous_boners.push(x);
+        }
+    }
+}
+
+for (let i=0; i<num+2; i++) {
+    random_bones();
     temp = "";
+    console.log(dangerous_boners);
 
     temp += "<tr>";
 
@@ -16,14 +31,25 @@ for (let i=0; i<num+2; i++) {
 
             temp += '<th style="width: '+spacing+'%">';
             if (!(j == 0 || j == num+1)) {
-                temp += '<img src="resources/bone.png" style="width:100%" id="bone'+id+'"/>';
+                
+                if (dangerous_boners.includes(id)) {
+                    temp += '<img src="resources/bone.png" style="width:100%" id="dangerous_boners'+id+'"/>';
+                }
+                
+                else {
+                    temp += '<img src="resources/bone.png" style="width:100%" id="bone'+id+'"/>';
+                }
                 id++;
             }
             temp += '</th>';
         }
         else if (i != num+1) {  // Content
             if (j == 0 || j == 1) {
-                temp += '<td> <img src="resources/bone.png" style="width:100%" id="bone'+id+'"/> </td>';
+                if (dangerous_boners.includes(id)) {
+                    temp += '<td> <img src="resources/bone.png" style="width:100%" id="dangerous_boners'+id+'"/> </td>';
+                } else {
+                    temp += '<td> <img src="resources/bone.png" style="width:100%" id="bone'+id+'"/> </td>';
+                }
                 id++;
             }
             if (j == 0 && i == 1) {  // Dog: id="dog"
@@ -33,7 +59,13 @@ for (let i=0; i<num+2; i++) {
         else {  // Footer
             temp += "<td>";
             if (!(j == 0 || j == num+1)) {
-                temp += '<img src="resources/bone.png" style="width:100%" id="bone'+id+'"/>';
+                if (dangerous_boners.includes(id)) {
+                    temp += '<img src="resources/bone.png" style="width:100%" id="dangerous_boners'+id+'"/>';
+                }
+                
+                else {
+                    temp += '<img src="resources/bone.png" style="width:100%" id="bone'+id+'"/>';
+                }
                 id++;
             }
             temp += "</td>";
@@ -45,3 +77,4 @@ for (let i=0; i<num+2; i++) {
 
     $("#game-content").append(temp);
 }
+
